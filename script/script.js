bd6afe4e464a0f1c0b1cd00bbda7d838f33d497a
Vladislav Kozhevatov <envydef@gmail.com>
2020-10-22 23:28:52 +0300

    fix:fix errors

--- script/script.js
+++ script/script.js
@@ -1,24 +1,25 @@
-let editBtn = document.querySelector('.edit-btn');
+let editBtn = document.querySelector('.profile__edit-btn');
 let closeBtn = document.querySelector('.popup__close-btn');
 let formElement = document.querySelector('.popup__container');
+let nameInput = document.querySelector('.popup__input-item_el_name');
+let jobInput = document.querySelector('.popup__input-item_el_job');
+let profileName = document.querySelector('.profile__name');
+let profileJob = document.querySelector('.profile__job');
+let popup = document.querySelector('.popup');
 
-function togglePopup() {
-    let popup = document.querySelector('.popup');
+function togglePopup() { 
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileJob.textContent;
     popup.classList.toggle('popup_opened');
-
 }
-editBtn.addEventListener('click', togglePopup);
-closeBtn.addEventListener('click', togglePopup);
 
 function formSubmitHandler(evt) {
     evt.preventDefault();
-    let nameInput = document.querySelector('.popup__name');
-    let jobInput = document.querySelector('.popup__job');
-    let profileName = document.querySelector('.profile__name');
-    let profileJob = document.querySelector('.profile__job');
     profileName.textContent = nameInput.value;
     profileJob.textContent = jobInput.value;
     togglePopup();
 }
 
+editBtn.addEventListener('click', togglePopup);
+closeBtn.addEventListener('click', togglePopup);
 formElement.addEventListener('submit', formSubmitHandler);
